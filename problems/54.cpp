c70f797382164aedb3a9fda035baad3e0e381e76
David Hamilton <davidhamiltron@gmail.com>
2019-06-04 20:53:34 +1200

    Adding solution and source file for poker problem.

---
+++ problems/54.cpp
@@ -0,0 +1,42 @@
+
+#include <fstream>
+#include "../Problems.h"
+#include "../Problem.h"
+#include "../Poker.h"
+
+using namespace std;
+
+void P54() {
+    AddProblem(
+        {
+            54, "How many hands does Player 1 win?",
+
+            []() -> string {
+
+                //Poker::Game g("8C TS KC 9H 4S 7D 2S 5D 3S AC");
+                //int p = g.Result();
+                //int answer = 0;
+
+                char hands[256];
+                int player1Won = 0;
+                ifstream inFile("D:\\programming\\euler\\problems\\54.txt");
+                if (!inFile) return "0";
+                int count = 1;
+                while (!inFile.eof())
+                {
+                    inFile.getline(hands, 256);
+                    Poker::Game g(hands);
+                    int result = g.Result();
+                    if (result == 1)
+                    {
+                        player1Won++;
+                    }
+                    cout << count << ": " << result << endl;
+                    
+                }
+                
+
+                return to_string(player1Won);
+            }
+        });
+};
